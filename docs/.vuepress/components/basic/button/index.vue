d29961749da4c0d8c458500909ed1ffe4a44ba06
zhangchao <529743595@qq.com>
2021-07-22 18:46:17 +0800

    feat: add example button

---
+++ docs/.vuepress/components/basic/button/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <main>
+      <h3>基础用法</h3>
+      <div class="block">
+        <div class="source">
+          <GupoButton>默认按钮</GupoButton>
+          <GupoButton type="primary">主要按钮</GupoButton>
+          <GupoButton type="success">成功按钮</GupoButton>
+          <GupoButton type="warning">警告按钮</GupoButton>
+          <GupoButton type="info">信息按钮</GupoButton>
+          <GupoButton type="error">失败按钮</GupoButton>
+        </div>
+        <div class="source">
+          <GupoButton round>圆角按钮</GupoButton>
+          <GupoButton round type="primary">主要按钮</GupoButton>
+          <GupoButton round type="success">成功按钮</GupoButton>
+          <GupoButton round type="warning">警告按钮</GupoButton>
+          <GupoButton round type="info">信息按钮</GupoButton>
+          <GupoButton round type="error">失败按钮</GupoButton>
+        </div>
+        <CodeBar title="使用 type、round 和 circle 属性来定义 Button 的样式。" :code="code1"></CodeBar>
+      </div>
+    </main>
+    <footer><Attr /></footer>
+  </div>
+</template>
+
+<script>
+import GupoButton from "../../../../../src/components/Basic/Button";
+import CodeBar from "../../utils/CodeBar";
+import Attr from './attr'
+
+export default {
+  name: 'ButtonExample',
+  components: {CodeBar, GupoButton, Attr},
+  data() {
+    return {
+      code1: `
+        <div>
+          <GupoButton>默认按钮</GupoButton>
+          <GupoButton type="primary">主要按钮</GupoButton>
+          <GupoButton type="success">成功按钮</GupoButton>
+          <GupoButton type="warning">警告按钮</GupoButton>
+          <GupoButton type="info">信息按钮</GupoButton>
+          <GupoButton type="error">失败按钮</GupoButton>
+        </div>
+        <div>
+          <GupoButton round>圆角按钮</GupoButton>
+          <GupoButton round type="primary">主要按钮</GupoButton>
+          <GupoButton round type="success">成功按钮</GupoButton>
+          <GupoButton round type="warning">警告按钮</GupoButton>
+          <GupoButton round type="info">信息按钮</GupoButton>
+          <GupoButton round type="error">失败按钮</GupoButton>
+        </div>
+        <div>
+          <GupoButton circle icon="search"></GupoButton>
+          <GupoButton circle type="info" icon="setting"></GupoButton>
+          <GupoButton circle type="success" icon="star"></GupoButton>
+          <GupoButton circle type="error" icon="delete"></GupoButton>
+        </div>
+      `.replace(/^ {8}/gm, '').trim(),
+    }
+  },
+}
+</script>
---
+++ docs/components/basic/button/README.md
@@ -0,0 +1,5 @@
+# Button 按钮
+
+<ClientOnly>
+  <basic-button-index></basic-button-index>
+</ClientOnly>
