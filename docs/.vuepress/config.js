95503eb4f15b96925e5d85e5cbb7d69be1b27590
zhangchao <529743595@qq.com>
2021-07-22 18:45:20 +0800

    feat: add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  base: '/gupo-front-components/',
+  title: 'gupo-front-components',
+  description: '前端组精心准备的 Vue 2.x 的组件库',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  plugins: ['@vuepress/back-to-top', '@vuepress/nprogress', '@vuepress/active-header-links'],
+  themeConfig: {
+    logo: '/logo.jpg',
+    displayAllHeaders: true,
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '组件', link: '/components/basic/button/' },
+      { text: 'npm', link: 'https://www.npmjs.com/package/', target:'_blank' },
+      { text: 'GitHub', link: 'https://github.com/', target:'_blank' },
+    ],
+    sidebar: {
+      '/components/': [
+        {
+          path: '/components/introduce/',
+          title: '开始',
+        },
+        {
+          title: '通用',
+          collapsable: false,
+          children: [
+            { title: 'Button 按钮', path: '/components/basic/button/' },
+          ]
+        },
+      ],
+    }
+  }
+}
