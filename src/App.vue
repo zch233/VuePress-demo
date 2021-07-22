fe177020e0036e8353f1a6b955377e7b2b2cbba7
zhangchao <529743595@qq.com>
2021-07-22 15:55:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  render() {
+    return <div id="app">
+      <HelloWorld />
+    </div>
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
