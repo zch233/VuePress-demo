6fba25b830b3ed6e0e1508707518f5eed0ac5e5d
zhangchao <529743595@qq.com>
2021-09-03 14:39:20 +0800

    docs: update README

--- src/main.js
+++ src/main.js
@@ -6,3 +6,7 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// 根据注释生成文档
+// https://segmentfault.com/a/1190000040260635
+// https://vuese.github.io/vuese-explorer/
